b11f3d498d23003897cb6b50deafdddb768b2fb5
OtabekRintaro <otabekrintaro@gmail.com>
2024-03-13 12:13:27 +0100

    Update index page of the server

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,4 @@
-from flask import Flask, json
+from flask import Flask, json, url_for
 
 from main import database
 from src.graph_response_parser import GraphResponseParser
@@ -9,7 +9,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Path Finding algos!</p>"
+    return "<p>Path Finding algos!</p>" + list_of_endpoints()
+
+
+def list_of_endpoints():
+    urls = list()
+    urls.append("<h4>API endpoints.</h4>")
+
+    urls.append("METHOD: DELETE, " + url_for('clear_graph'))
+    urls.append("METHOD: GET, " + url_for('get_nodes'))
+    urls.append("METHOD: POST, " + url_for('add_node'))
+    urls.append("METHOD: DELETE, " + url_for('remove_node', node_id="node_id"))
+    urls.append("METHOD: POST, " + url_for('add_edge', node_id1="first_node_id", node_id2="second_node_id"))
+    urls.append("METHOD: DELETE, " + url_for('remove_edge', node_id1="first_node_id", node_id2="second_node_id"))
+
+    for i in range(1, len(urls)):
+        urls[i] = f'{urls[i]} <br/><br/>'
+
+    return "".join(urls)
 
 
 @app.route("/graph", methods=["DELETE"])
